25c09cea59359972b97e3192ad3246127058b799
nikobez <nikobez@yandex.ru>
2021-06-15 09:30:00 +0300

    no comments

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
 # This is a sample Python script.
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 #
-#def print_hi(name):
-#    # Use a breakpoint in the code line below to debug your script.
-#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-# Press the green button in the gutter to run the script.
-#if __name__ == '__main__':
-#    print_hi('PyCharm')
-#
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 from ctypes import *
+from PyQt5 import QtWidgets, uic
+import sys
 
-lib = cdll.LoadLibrary(r"C:\Windows\System32\msvcrt.dll")
-lib2 = cdll.LoadLibrary(r"C:\ICPDAS\UniDAQ\driver\DLL\x64\UniDAQ.dll")
-
-var_c = 51
-print_from_C = getattr(lib, "printf")  # да, тут можно вписать даже "??2@YAPAXI@Z"
-print_from_C(b"Print int_c = %d\n", var_c)
+#lib = cdll.LoadLibrary(r"C:\ICPDAS\UniDAQ\driver\DLL\x64\UniDAQ.dll")
 
 #Public Function Ixud_GetDllVersion(ByRef dwDLLver As UInt32) As UInt16
+#
+#Ixud_GetDllVersion = getattr(lib2, "Ixud_GetDllVersion")
+#version = Ixud_GetDllVersion()
+#
+#print(">>", version)
 
-Ixud_GetDllVersion = getattr(lib2, "Ixud_GetDllVersion")
-version = Ixud_GetDllVersion()
+app = QtWidgets.QApplication([])
+win = uic.loadUi("forms/mainform.ui")
 
-print(">>", version)
+win.show()
+sys.exit(app.exec())
